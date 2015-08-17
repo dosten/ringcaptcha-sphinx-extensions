2a058fc2242662156666e8d8fdbd47936873d1a5
Diego Saint Esteben <diego@saintesteben.me>
2015-08-17 12:18:07 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'ringcaptcha-sphinx-extensions',
+    version = '1.0',
+    author = 'Diego Saint Esteben',
+    author_email = 'diego@saintesteben.me',
+    license = 'Apache-2.0',
+    packages = find_packages(),
+)
